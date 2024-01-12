3adaaa4097f7025535521459aa6d17ab36c50abd
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-12 20:44:06 +0530

    Update

---
+++ portfolio.js
@@ -0,0 +1,32 @@
+// var main = document.querySelector("#main");
+// var cursor = document.querySelector(".cursor");
+// // console.log(main);
+// main.addEventListener("mousemove",function(e){
+//     // console.log("Hye");
+//     cursor.style.left = e.x + "px"; 
+//     cursor.style.top = e.y + "px";
+// })
+
+var navbar = document.querySelector(".navbar");
+
+var navicon = document.querySelector(".navicon");
+
+var navlist = document.querySelector(".navlist");
+// console.log(navlist);
+
+var flag =0 ;
+// navicon.addEventListener("click",function(){
+//     if(flag === 0 ){
+//         navlist.style.display = "block"; 
+//         navlist.style.height = "100%";
+//         navlist.style.paddingLeft = "20%";
+//         flag =1 ;
+//     }
+//     else{
+//         navlist.style.display = "none";
+//         navbar.style.height = "auto"; 
+//         flag = 0 ;
+//     }
+//     // console.log(e);
+//     // navbar.style.width = "100vmax";
+// })
