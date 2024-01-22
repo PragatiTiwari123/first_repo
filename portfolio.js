9dd90ed3bf54adf5a6795d9c2d72b9fbcbf90eb2
PragatiTiwari123 <pragatip963@gmail.com>
2024-01-22 21:44:26 +0530

    add changes

--- portfolio.js
+++ portfolio.js
@@ -11,27 +11,18 @@ var navbar = document.querySelector(".navbar");
 
 var navicon = document.querySelector(".navicon");
 
-var navlist = document.querySelector(".navlist");
+var vresp = document.querySelector("#v-nav-resp");
 // console.log(navlist);
+var navList = document.querySelectorAll(".navlist");
 
 var flag =0 ;
-// navicon.addEventListener("click",function(){
-//     if(flag === 0 ){
-//         navlist.style.display = "block"; 
-//         navlist.style.height = "100%";
-//         navlist.style.paddingLeft = "20%";
-//         flag =1 ;
-//     }
-//     else{
-//         navlist.style.display = "none";
-//         navbar.style.height = "auto"; 
-//         flag = 0 ;
-//     }
-//     // console.log(e);
-//     // navbar.style.width = "100vmax";
-// })
+navicon.addEventListener("click",function(e){
+    // navbar.classList.toggle('v-nav-resp')
+    vresp.classList.toggle('.navlist')
+    // console.log(e);
+})
 
-var navList = document.querySelectorAll(".navlist");
+
 // console.log(navList);
 // console.log(navList.children);
 navList.forEach(function(e){
